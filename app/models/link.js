dd1ad8798ff126809d73c2577d9d3d8778dbe960
Hack Reactor Students <accounts@hackreactor.com>
2016-08-25 16:36:11 -0700

    auto commit

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,23 @@
 var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var urlsSchema = mongoose.Schema({
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number,
+
+});
+
+var Link = mongoose.model('Link', urlsSchema);
+
+urlsSchema.pre('save', function(next) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
 });
 
 module.exports = Link;
