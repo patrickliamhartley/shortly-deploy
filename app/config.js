b31f6bdac6d7c3a719a84bec9d88dc9cab0fdf06
Hack Reactor Students <accounts@hackreactor.com>
2016-08-25 17:35:06 -0700

    auto commit

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://198.199.97.92/data');
+var db = mongoose.connect('mongodb://127.0.0.1:27017');
 
 db.on('error', console.error.bind(console, 'error: '));
 db.once('open', function() {
